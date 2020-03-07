96677537b3eb95fbcb0b701f14cbe3ef1309c41a
marina <alvarez.marina7@hotmail.fr>
2020-03-07 17:58:40 +0100

    init

--- app/server/app.js
+++ app/server/app.js
@@ -20,7 +20,7 @@ mongoose
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error('Error connecting to mongo ff', err)
   });
 
 const app_name = require('./package.json').name;
